4f8091d40e8a91c12830a7e8bfa9b72ce36f91ee
Renaranananna <Renato.andaur@gmail.com>
2022-10-06 10:40:29 -0300

    Add calcular el centro y dibujar contorno

--- deteccion.py
+++ deteccion.py
@@ -3,12 +3,12 @@ import numpy as np
 from cv2 import cv2
 import matplotlib as plt
 
-img = cv2.imread("fotos/circulo.png")
+img = cv2.imread("fotos/cuadrado.png")
 
 img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
 # Filtrar con un Blur Gaussiano
-kernel = (7, 7)
+kernel = (11, 11)
 img_gaus = cv2.GaussianBlur(img_gray, kernel, 0)
 
 # Aplicar filtro Canny para detección de bordes
@@ -18,30 +18,31 @@ img_canny = cv2.Canny(img_gaus, 10, 100)
 img_filtered = cv2.dilate(img_canny, (2,2))
 img_filtered = cv2.erode(img_filtered, (2,2))
 
+#cv2.imshow("prueba", img_canny)
+
 # Encontrar contornos
 contours, hierarchy = cv2.findContours(img_filtered, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
 
-# Filtrar los contornos obtenidos para quedarse sólo con aquellos cuya área sea suficientemente grande (>10.000px)
+# Filtrar los contornos obtenidos para quedarse sólo con aquellos cuya área sea suficientemente grande (>1000px)
 contornos_buenos = []
 for c in contours:
-    if cv2.contourArea(c) >= 10000:
+    if cv2.contourArea(c) >= 1000:
         contornos_buenos.append(c)
 
 contours = contornos_buenos
 
+cv2.drawContours(img, contours, -1, (0,0,255), 6)
 for c in contours:
+    #Sacamos el centro de la imagen
+    M = cv2.moments(c)
+    if M['m00'] != 0:
+        #cx y cy es el centro y usamos esto pa mover al roboc?
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+        cv2.circle(img, (cx, cy),0,  (0,255,0), 5)
 
-    # Calcular la cantidad de lados de la forma
-    """ En esta sección se recomienda consultar el sitio https://www.pyimagesearch.com/2016/02/08/opencv-shape-detection/ """
-    perimetro = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.04 * perimetro, True)
-    shape_size = len(approx)
-    # Dibujar bounding box del contorno y agregar texto que lo describe
-    x, y, w, h = cv2.boundingRect(approx)
-    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 5)
-
-
+#mostramos la imagen
 cv2.imshow("imagen", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
